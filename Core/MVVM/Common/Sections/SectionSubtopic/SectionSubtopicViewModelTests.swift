ca38592d7d17092fd0bb77227054751bfab98704
Oleg Petrychuk <petrychuk@mlsdev.com>
2018-09-11 19:14:20 +0300

    Updated SectionsViewModel tests

---
+++ Core/MVVM/Common/Sections/SectionSubtopic/SectionSubtopicViewModelTests.swift
@@ -0,0 +1,9 @@
+//
+//  SectionSubtopicViewModelTests.swift
+//  Core
+//
+//  Created by Петрічук Олег Аркадійовіч on 11.09.2018.
+//  Copyright © 2018 Oleg Petrychuk. All rights reserved.
+//
+
+import Foundation
