1b4ce7ea2dba98072f2db82b32580a06bf5afddb
Oleg Petrychuk <petrychuk@mlsdev.com>
2018-09-12 10:47:18 +0300

    Updated AppViewModel tests

--- Core/MVVM/App/AppViewModelTests.swift
+++ Core/MVVM/App/AppViewModelTests.swift
@@ -6,46 +6,43 @@
 //  Copyright © 2018 Oleg Petrychuk. All rights reserved.
 //
 
-import Quick
-import Nimble
-import Core
 import TestsHelper
 
-//class AppViewModelTests: BaseTestCase {
-//    override func spec() {
-//        describe("AppViewModel") {
-//            var sut: AppViewModel!
-//            let userStorage = UserStorageMock(isOnboardingPassed: BehaviorRelay(value: false))
-//            
-//            beforeEach {
-//                sut = AppViewModelFactory.default(userStorage: userStorage)
-//            }
-//            
-//            describe("when did become active trigger", {
-//                let shouldRouteTutorialObserver = self.scheduler.createObserver(Void.self)
-//                let shouldRouteAppObserver = self.scheduler.createObserver(Void.self)
-//                beforeEach {
-//                    sut.shouldRouteTutorial.drive(shouldRouteTutorialObserver).disposed(by: self.disposeBag)
-//                    sut.shouldRouteApp.drive(shouldRouteAppObserver).disposed(by: self.disposeBag)
-//                }
-//                
-//                context("and onboarding is passed") {
-//                    it("should route to app", closure: {
-//                        userStorage.isOnboardingPassed.accept(true)
-//                        sut.didBecomeActiveTrigger.onNext(())
-//                        expect(shouldRouteTutorialObserver.events.count).to(equal(0))
-//                        expect(shouldRouteAppObserver.events.count).to(equal(1))
-//                    })
-//                }
-//                context("and onboarding is not passed") {
-//                    it("should route to tutorial", closure: {
-//                        userStorage.isOnboardingPassed.accept(false)
-//                        sut.didBecomeActiveTrigger.onNext(())
-//                        expect(shouldRouteTutorialObserver.events.count).to(equal(1))
-//                        expect(shouldRouteAppObserver.events.count).to(equal(1))
-//                    })
-//                }
-//            })
-//        }
-//    }
-//}
+class AppViewModelTests: BaseTestCase {
+    override func spec() {
+        describe("AppViewModel") {
+            var sut: AppViewModel!
+            let userStorage = UserStorageMock(isOnboardingPassed: BehaviorRelay(value: false), email: BehaviorRelay(value: nil))
+            
+            beforeEach {
+                sut = AppViewModelImpl(userStorage: userStorage)
+            }
+            
+            describe("when did become active trigger", {
+                let tutorialObserver = self.scheduler.createObserver(Void.self)
+                let appObserver = self.scheduler.createObserver(Void.self)
+                beforeEach {
+                    sut.shouldRouteTutorial.drive(tutorialObserver).disposed(by: self.disposeBag)
+                    sut.shouldRouteApp.drive(appObserver).disposed(by: self.disposeBag)
+                }
+                
+                context("and onboarding is passed") {
+                    it("should route to app", closure: {
+                        userStorage.isOnboardingPassed.accept(true)
+                        sut.didBecomeActiveTrigger.onNext(())
+                        expect(tutorialObserver.events.count).to(equal(0))
+                        expect(appObserver.events.count).to(equal(1))
+                    })
+                }
+                context("and onboarding is not passed") {
+                    it("should route to tutorial", closure: {
+                        userStorage.isOnboardingPassed.accept(false)
+                        sut.didBecomeActiveTrigger.onNext(())
+                        expect(tutorialObserver.events.count).to(equal(1))
+                        expect(appObserver.events.count).to(equal(1))
+                    })
+                }
+            })
+        }
+    }
+}
